229ce78957e4ae93fea009502289b8c41c79658c
gabrielrowan <gabrielrowan82@gmail.com>
2024-12-19 16:08:57 +0000

    almost there!

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-let date = new Date();
-let year = date.getFullYear();
-
-let yearTextNode = document.createTextNode(year);
-document.getElementById("copyright-year").appendChild(yearTextNode);
 
 
-const hamburger = document.querySelector(".hamburger-menu");
-const navMenu = document.querySelector(".nav-menu");
+toggleHamburgerMenu();
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
+document.addEventListener("DOMContentLoaded", () =>
+{
+    setCopywriteYear();
+    applyAnimationToIconContainers();
+
+});
+
+
+
+function toggleHamburgerMenu()
+{
+    const hamburger = document.querySelector(".hamburger-menu");
+    const navMenu = document.querySelector(".nav-menu");
+
+    hamburger.addEventListener("click", () =>
+    {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    });
+}
+
+function setCopywriteYear()
+{
+    let date = new Date();
+    let year = date.getFullYear();
+
+    let yearTextNode = document.createTextNode(year);
+    document.getElementById("copyright-year").appendChild(yearTextNode);
+}
+
+function applyAnimationToIconContainers()
+{
+    const iconContainers = document.querySelectorAll(".icon-container");
+    const delay = 1000;
+
+    iconContainers.forEach((container, index) =>
+    {
+        setTimeout(() =>
+        {
+            container.classList.add("active");
+
+        }, delay * (index + 1));
+    });
+}
+
